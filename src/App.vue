c33df73382ab446072b5b9b2e50e73ab37406c2a
Zhao_sj <1640945861@qq.com>
2024-01-21 16:53:00 +0800

    commit bug

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+import { NDataTable } from "naive-ui";
+
+interface RowData {
+  id: number;
+  name: string;
+}
+
+const columns = [
+  {
+    key: "id",
+    title: "ID",
+  },
+  {
+    key: "name",
+    title: "Name",
+  },
+];
+
+const data: RowData[] = [];
+
+for (let i = 0; i < 100; i++) {
+  data.push({
+    id: i + 1,
+    name: `Name ${i}`,
+  });
+}
+</script>
+
+<template>
+  <div>
+    <NDataTable
+      :columns="columns"
+      :data="data"
+      :max-height="500"
+      virtual-scroll
+    />
+  </div>
+</template>
+
+<style scoped></style>
